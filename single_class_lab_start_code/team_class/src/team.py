4acdb4cd99c1f590efc6a544449e4b09b3ac738a
linabiel <linuka.b@gmail.com>
2021-04-05 15:27:45 +0100

    reformated the code

--- single_class_lab_start_code/team_class/src/team.py
+++ single_class_lab_start_code/team_class/src/team.py
@@ -3,19 +3,16 @@ class Team:
         self.name = name
         self.players = players
         self.coach = coach
-
         self.points = 0
 
     def add_player(self, new_player_name):
         self.players.append(new_player_name)
 
     def has_player(self, player_name):
-
         if player_name in self.players:
             # for player in self.players:
             #     if player == player_name:
             return True
-
         return False
 
     def play_game(self, did_team_win):
